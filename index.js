1ca08efda6bf5063acc5ab497b89fd0c00746a50
austin sanchez <austinsanchez45@yahoo.com>
2021-12-08 09:58:12 -0600

    Created index.css and added some fonts

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const jsxElement = <h1>Our React App</h1>;
+console.log(jsxElement);
+
+reactDom.render(jsxElement, document.getElementById('root'));
