84e8129e53e01cc045ef6a75b0121daf41d20d47
austin sanchez <austinsanchez45@yahoo.com>
2021-12-09 04:20:29 -0600

    installed jwtdecode to decode tokens, set up function in App.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import jwt_decode from 'jwt-decode';
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+    //Function to get local login if needed
+    const [user, setUser] = useState(null);
+    const jwt = localStorage.getItem('token');
+    useEffect(() => {
+        try{
+            const localUser = jwtDecode(jwt);
+            setUser(localUser);
+        }
+        catch{}
+    }, [])
+}
