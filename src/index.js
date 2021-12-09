50cf0b745275b6d85ebe2cf945ba2f2cf8fa5828
austin sanchez <austinsanchez45@yahoo.com>
2021-12-09 16:14:20 -0600

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import reactDom from "react-dom";
 import App from "./Components/App"
+import { BrowserRouter as Router } from "react-router-dom";
 
 const jsxElement = <h1>Our React App</h1>;
 console.log(jsxElement);
 
-reactDom.render(<App />, document.getElementById('root'));
+reactDom.render(
+<Router>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+</Router>,
+document.getElementById('root')
+);
